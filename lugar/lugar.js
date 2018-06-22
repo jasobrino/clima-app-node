b0ead09df02c7853a53817351941b7d0b741de59
jasobrino <jasobrino66@gmail.com>
2018-06-22 13:50:08 +0200

    varios

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -4,22 +4,23 @@ const rp = require('request-promise');
 //procedimiento de llamada al api de maps con el paquete axios
 const getLugarLatLng = async(direccion) => {
 
-        let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyDzbQ_553v-n8QNs2aafN9QaZbByTyM7gQ`);
+    let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyDzbQ_553v-n8QNs2aafN9QaZbByTyM7gQ`);
 
-        if (resp.data.status === 'ZERO_RESULTS') {
-            throw new Error(`No hay resultados para la ciudad ${ direccion }`);
-        }
-
-        let data = resp.data.results[0];
-        let coord = data.geometry.location;
-
-        return {
-            direccion: data.formatted_address,
-            lat: coord.lat,
-            lng: coord.lng
-        }
+    if (resp.data.status === 'ZERO_RESULTS') {
+        throw new Error(`No hay resultados para la ciudad ${ direccion }`);
     }
-    //procedimiento de llamada al api de maps con el paquete request
+
+    let data = resp.data.results[0];
+    let coord = data.geometry.location;
+
+    return {
+        direccion: data.formatted_address,
+        lat: coord.lat,
+        lng: coord.lng
+    }
+}
+
+//procedimiento de llamada al api de maps con el paquete request
 const getLugarLatLngReq = async(direccion) => {
 
     let encodeUrl = encodeURI(direccion);
