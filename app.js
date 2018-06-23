8c7cff76058e8b00aaf563577854f07fa9174903
Juan <jasobrino66@gmail.com>
2018-06-23 17:12:06 +0200

    correcciones varias

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ let getInfo = async(direccion) => {
     try {
         //hasta que no temina la llamada asíncrona no continúa la ejecución
         let coors = await lugar.getLugarLatLng(direccion);
+        console.log(coors);
         let dclima = await clima.getClima(coors.lat, coors.lng);
+        console.log(dclima);
 
         return `El clima en ${ coors.direccion } es de ${ dclima.temp } con ${ dclima.humedad }% de humedad `;
 
--- lugar/lugar.js
+++ lugar/lugar.js
@@ -4,6 +4,7 @@ const rp = require('request-promise');
 //procedimiento de llamada al api de maps con el paquete axios
 const getLugarLatLng = async(direccion) => {
 
+    let encodeUrl = encodeURI(direccion);
     let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyDzbQ_553v-n8QNs2aafN9QaZbByTyM7gQ`);
 
     if (resp.data.status === 'ZERO_RESULTS') {
